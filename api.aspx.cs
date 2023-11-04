c4dde1768d2c913ccfd3d1b9b1df7cc272677997
makaeenm <bachduyatk300@gmail.com>
2023-11-04 22:09:25 +0700

    Xong login nhưng khi sai mật khẩu vẫn bị đóng dialog

--- api.aspx.cs
+++ api.aspx.cs
@@ -16,25 +16,28 @@ namespace doan
         {
             SqlServer db = new SqlServer();
             SqlCommand cmd = db.GetCmd("ql_don", action);
-            switch (action)
-            {
-                case "them_don_hang":
-                    cmd.Parameters.Add("@makh", SqlDbType.NVarChar, 255).Value = Request["Mã khách hàng"];
-                    cmd.Parameters.Add("@mabanh", SqlDbType.NVarChar, 255).Value = Request["Mã bánh"];
-                    cmd.Parameters.Add("@tongtien", SqlDbType.Float).Value = Request["Tổng tiền"];
-                    break;
-            }
             string json = (string)db.Scalar(cmd);
             Response.Write(json);
         }
+
+        void logins(string action)
+        {
+            SqlServer db = new SqlServer();
+            SqlCommand cmd = db.GetCmd("ql_taikhoan", action);
+            string json = (string)db.Scalar(cmd);
+            Response.Write(json);//thang nay xuot du lieu trong db thanh chuoi trong json
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
             string action = Request["action"];
             switch(action)
             {
                 case "list_don_hang":
-                case "them_don_hang":
                     xuly_donhang(action);
+                    break;
+                case "logins":
+                    logins(action);
                     break;
             }
         }
--- asset/Codejs.js
+++ asset/Codejs.js
@@ -1,5 +1,72 @@
 ﻿$(document).ready(function () {
-    const api ='/api.aspx'
+    const api = '/api.aspx'
+    //------------------------------------------------------------------
+    $('#btn-login').click(function () {
+        $.confirm({
+            
+            title: "Đăng nhập",
+            content: ``
+                + `<form action="" class="form_name">` +
+                `<div class="form-group">` +
+                `<label>Tài khoản</label>` +
+                `<input type="text" placeholder="Tài khoản" class="tk form-control" required />` +
+                `<label>Mật khẩu</label>` +
+                `<input type="text" placeholder="Mật khẩu" class="mk form-control" required />` +
+                `</div` +
+                `</form>`
+            ,
+            buttons: {
+                formSumbit: {
+                    text: 'Đăng nhập',
+                    btnClass: 'btn-blue',
+                    action: function () {
+                        var tkb = this.$content.find('.tk').val();
+                        var mkb = this.$content.find('.mk').val();
+                        if (!tkb || !mkb) {
+                            $.alert('Bạn chưa điền gì cả !');
+                            return false;
+                        }
+                        else {
+                            $.post(api, { action: 'logins' },
+                                function (data) {
+                                    //alert(data)//{"ok":1,"msg":"ok","data":[{"tk":admin,"mk":"123"}]}
+                                    var json = JSON.parse(data);//chuyen text trong data -> doi tuonng 
+                                    var loginok = false;
+                                    if (json.ok) {
+                                        for (var tt of json.data) {
+                                            if (tt.tk == tkb && tt.mk == mkb) {
+                                                loginok = true;
+                                                break;
+                                            }
+                                        }
+                                        if (loginok) {
+                                            $.alert("Đăng nhập thành công!");
+                                        }
+                                        else {
+                                            $.alert("Sai tài khoản hoặc mật khẩu!");
+                                            return false;
+                                            
+                                        }
+                                    }
+                                })
+                        }
+                    }
+                },
+                somethingElse: {
+                    text: 'Thoát',
+                    btnClass: 'btn-red' 
+                }
+            },
+            onContentReady: function () {
+                
+            }
+        })
+    });
+
+
+
+
+    //--------------------------------------------------------------
     $('#xem-don').click(function () {
         //alert('hehehe')
         $.confirm({
@@ -8,9 +75,20 @@
             ColumnClass: 'large',
             onContentReady: function () {
                 $.post(api, { action: 'list_don_hang' },
-                function (data) {
-                    alert(data)
-                    var noidung = "solo yasuo "
+                    function (data) {
+                        //alert(data)
+                        var json = JSON.parse(data);
+                        var noidung = "";
+                        if (json.ok) {
+                            noidung += "<table>";
+                            for (var dh of json.data) {//moi 1 don hang trong json thi lam gi (lay thuoc tinh moi don hang )
+                                noidung += `Mã đơn: ${dh.madon}<br>Mã khách hàng: ${dh.makh}<br>`
+                            }
+                            noidung += "</table>"
+                        }
+                        else {
+                            noidung = " khong co du lieu";
+                        }
                     $('#ds_don_hang').html(noidung);
                 })
             }
